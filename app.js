72e95ee96358a1bb3b13009bd6e6d71a698dce62
Hanssen <hramanjooloo@gmail.com>
2020-07-25 20:23:46 +0100

    added success and failure pages

--- app.js
+++ app.js
@@ -15,13 +15,59 @@ app.get("/", function(req, res) {
 
 app.post("/", function(req, res) {
        
-    let firstName = req.body.firstName;
-    let secondName = req.body.secondName;
-    let emailAddress = req.body.email;
+    const firstName = req.body.firstName;
+    const lastName = req.body.lastName;
+    const emailAddress = req.body.email;
 
-    console.log(firstName, secondName, emailAddress);    
+    const data = {
+        members: [
+            {
+                email_address: emailAddress,
+                status: "subscribed",
+                merge_fields: {
+                    FNAME: firstName,
+                    LNAME: lastName
+                }
+            }
+        ]
+    }
+
+    const jsonData = JSON.stringify(data);
+
+    const url = "https://us17.api.mailchimp.com/3.0/lists/734d4df17c";
+
+    const options = {
+        method: "POST",
+        auth: "hanssen7:54f83092b078ce9f125a6384026515d4-us17"
+    }
+    
+    const request = https.request(url, options, function(response) {
+        if (response.statusCode === 200) {
+            res.sendfile(__dirname + "/success.html")
+        } else {
+            res.sendfile(__dirname + "/failure.html")
+        }
+
+        response.on("data", function(data) {
+            console.log(JSON.parse(data));
+        })
+    })
+
+    // request.write(jsonData);
+    request.end();
+
 })
 
+
+// API Key
+// 54f83092b078ce9f125a6384026515d4-us17
+
+// MailChimp List ID
+// 734d4df17c
+
+app.post("/failure", function(req, res) {
+    res.redirect("/");
+})
 
 
 app.listen(3000, function() {
