a59ed8b1b9dfab4ffb2df828c68e6d9cc0e36a73
Hanssen <hramanjooloo@gmail.com>
2020-07-25 16:37:35 +0100

    sign up page setup

--- app.js
+++ app.js
@@ -4,7 +4,23 @@ const https = require("https");
 
 const app = express();
 
+app.use(bodyParser.urlencoded({extended: true}));
 
+// Use a static folder to load static files
+app.use(express.static("public"));
+
+app.get("/", function(req, res) {
+    res.sendFile(__dirname + "/signup.html");
+})
+
+app.post("/", function(req, res) {
+       
+    let firstName = req.body.firstName;
+    let secondName = req.body.secondName;
+    let emailAddress = req.body.email;
+
+    console.log(firstName, secondName, emailAddress);    
+})
 
 
 
